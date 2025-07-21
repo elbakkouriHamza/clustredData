da64598c39418d06eedec77d2e9cdfbf45b33b6c
hamzaelbakkouri <hamza.elbakkouri@marketingconfort.com>
2025-07-21 20:43:45 +0100

    setup/setup ClusteredData Warehouse project

---
+++ src/test/java/com/app/clustereddata/ClusteredDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.clustereddata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
